b961f570dbf8ad52afa4fc19e85189bb2000d045
shlan <shlan1208@gmail.com>
2016-04-19 18:19:25 -0700

    working number 1 lab exercise

---
+++ app/src/androidTest/java/com/example/vguan/lab3/tests/JUnit_test.java
@@ -0,0 +1,45 @@
+package com.example.vguan.lab3.tests;
+
+import com.example.vguan.lab3.MainActivity;
+import com.example.vguan.lab3.R;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.test.UiThreadTest;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/**
+ * Created by vguan on 4/19/2016.
+ */
+public class JUnit_test extends ActivityInstrumentationTestCase2<MainActivity>{
+
+    MainActivity mainActivity;
+    public JUnit_test() {
+
+        super(MainActivity.class);
+    }
+
+    @UiThreadTest
+    public void test_sum() {
+        mainActivity = getActivity();
+
+        Button button = (Button) mainActivity.findViewById(R.id.button1);
+
+        EditText one = (EditText) mainActivity.findViewById(R.id.num1);
+        one.setText("5");
+
+        EditText two = (EditText) mainActivity.findViewById(R.id.num2);
+        two.setText("10");
+
+        button.performClick();
+
+        TextView textView = (TextView) mainActivity.findViewById(R.id.sum);
+        int tester = Integer.parseInt(textView.getText().toString()); //taking xml id sum's integer value
+
+        int num1 = Integer.parseInt(one.getText().toString());
+        int num2 = Integer.parseInt(two.getText().toString());
+
+        assertEquals(num1+num2, tester);
+    }
+}
---
+++ app/src/main/java/com/example/vguan/lab3/MainActivity.java
@@ -0,0 +1,146 @@
+package com.example.vguan.lab3;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class MainActivity extends AppCompatActivity implements OnClickListener {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+    Button add;
+    TextView final_sum;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+
+        ///////////////////////////////////// MY STUFF ////////////////////////////////////
+
+        // creating the add button --------------------------------------------------
+        add = (Button)findViewById(R.id.button1);
+        final_sum = (TextView)findViewById(R.id.sum);
+
+        // set on click listeners on the buttons ------------------------------------
+        add.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.vguan.lab3/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.example.vguan.lab3/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+
+    ////////////////////////////////// MY STUFF //////////////////////////////////////////
+
+    public int sum() {
+        int num1 = Integer.parseInt(((EditText) findViewById(R.id.num1)).getText().toString());
+        int num2 = Integer.parseInt(((EditText) findViewById(R.id.num2)).getText().toString());
+        return num1 + num2;
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == add) {
+            int sum = sum();
+            final_sum.setText(Integer.toString(sum));
+            final_sum.setBackgroundColor(Color.CYAN);
+        }
+    }
+
+}
